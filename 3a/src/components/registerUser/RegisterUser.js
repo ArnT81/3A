b4d9b4399635f8bb8bc633c83e87c13864498f8b
Adnan <adnan.rruka@gmail.com>
2020-04-15 18:54:23 +0200

    Added validation in register component

--- 3a/src/components/registerUser/RegisterUser.js
+++ 3a/src/components/registerUser/RegisterUser.js
@@ -4,17 +4,22 @@ import { firebase, auth, db } from '../../firebase'
 import Wrapper from '../wrapper/Wrapper'
 import { Link } from 'react-router-dom'
 import styles from './registerUser.module.css'
+import { useForm } from 'react-hook-form'
 
 
 const RegistertUser = (props) => {
     const store = useContext(ThemeContext);
-    const register = (e) => {
-        e.preventDefault();
+    const { register, errors, handleSubmit } = useForm()
+
+
+    const registerNewUser = (data) => {
+        console.log(data.firstname)
+        // e.preventDefault();
         db.collection('Users').add({
-            firstname: e.target.firstname.value,
-            lastname: e.target.lastname.value,
-            username: e.target.username.value,
-            password: e.target.password.value,
+            firstname: data.firstname,
+            lastname: data.lastname,
+            username: data.username,
+            password: data.password,
         });
         store.user.set(true);
     }
@@ -24,22 +29,26 @@ const RegistertUser = (props) => {
         <Wrapper>
             <div className={styles.registerUser} style={{ color: store.color.get }}>
                 <p>Register new user</p>
-                <form name="form" onSubmit={register}>
+                <form name="form" onSubmit={handleSubmit(registerNewUser)} >
                     <div >
                         <label htmlFor="firstname">First Name:</label>
-                        <input placeholder="You'r First Name Here" type="text" name="firstname" />
+                        <input ref={register({ required: true, minLength: 6 })} placeholder="You'r First Name Here" type="text" name="firstname" />
+                        {errors.firstname && <p className={styles.errors}>First name is required</p>}
                     </div>
                     <div>
                         <label htmlFor="lastname">Last Name:</label>
-                        <input placeholder="You'r Last Name Here" type="text" name="lastname" />
+                        <input ref={register({ required: true, minLength: 6 })} placeholder="You'r Last Name Here" type="text" name="lastname" />
+                        {errors.lastname && <p className={styles.errors}> Last name is required</p>}
                     </div>
                     <div>
                         <label htmlFor="username">Username:</label>
-                        <input placeholder="You'r User Name Here" type="text" name="username" />
+                        <input ref={register({ required: true, minLength: 6 })} placeholder="You'r User Name Here" type="text" name="username" />
+                        {errors.username && <p className={styles.errors}> User name is required</p>}
                     </div>
                     <div>
                         <label htmlFor="password">Password:</label>
-                        <input placeholder="You'r Password Here" type="password" name="password" />
+                        <input ref={register({ required: true, minLength: 6 })} placeholder="You'r Password Here" type="password" name="password" />
+                        {errors.password && <p className={styles.errors}> Password is required</p>}
                     </div>
                     <div >
                         <button type="submit" >Register</button>
